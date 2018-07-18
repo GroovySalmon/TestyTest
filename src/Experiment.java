5bb46c791b1690451fef1860cc3e180cc804f9ab
GroovySalmon <34165484+GroovySalmon@users.noreply.github.com>
2018-07-18 14:50:12 +0200

    First commit to GITHUB

--- src/Experiment.java
+++ src/Experiment.java
@@ -3,5 +3,7 @@ public class Experiment {
     public static void main (String[]args) {
 
         System.out.println("this is a god damn test MOFOS");
+
+        System.out.println("another hello world");
     }
 }
